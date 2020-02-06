1aa8e647aa2e17b15f4ffd49a9de369a27dda43d
Karan Gujral <karan@sixsprints.com>
2020-02-06 19:00:10 +0530

    updated spring boot and jwt lib

--- src/main/java/com/sixsprints/core/utils/AuthUtil.java
+++ src/main/java/com/sixsprints/core/utils/AuthUtil.java
@@ -1,7 +1,7 @@
 
 package com.sixsprints.core.utils;
 
-import java.text.ParseException;
+import java.security.SecureRandom;
 import java.util.Date;
 
 import org.joda.time.DateTime;
@@ -10,9 +10,11 @@ import com.nimbusds.jose.JOSEException;
 import com.nimbusds.jose.JWSAlgorithm;
 import com.nimbusds.jose.JWSHeader;
 import com.nimbusds.jose.JWSSigner;
+import com.nimbusds.jose.JWSVerifier;
+import com.nimbusds.jose.KeyLengthException;
 import com.nimbusds.jose.crypto.MACSigner;
+import com.nimbusds.jose.crypto.MACVerifier;
 import com.nimbusds.jwt.JWTClaimsSet;
-import com.nimbusds.jwt.ReadOnlyJWTClaimsSet;
 import com.nimbusds.jwt.SignedJWT;
 import com.sixsprints.core.exception.NotAuthorizedException;
 
@@ -27,6 +29,14 @@ public class AuthUtil {
 
   private static final String ISSUER = "https://www.website.com";
 
+  private static SecureRandom random = new SecureRandom();
+
+  private static byte[] sharedSecret = new byte[32];
+
+  static {
+    random.nextBytes(sharedSecret);
+  }
+
   public static String decodeToken(String authHeader) throws NotAuthorizedException {
     return xor(decode(authHeader).getSubject().getBytes());
   }
@@ -36,41 +46,49 @@ public class AuthUtil {
   }
 
   public static String createToken(String subject, int expiryDays) {
-    JWTClaimsSet claim = new JWTClaimsSet();
-    claim.setSubject(xor(subject.getBytes()));
-    claim.setIssuer(ISSUER);
-    claim.setIssueTime(DateTime.now().toDate());
-    claim.setExpirationTime(DateTime.now().plusDays(expiryDays).toDate());
-    JWSSigner signer = new MACSigner(TOKEN_SECRET);
-    SignedJWT jwt = new SignedJWT(JWT_HEADER, claim);
+    JWSSigner signer = null;
     try {
-      jwt.sign(signer);
+      signer = new MACSigner(sharedSecret);
+    } catch (KeyLengthException e) {
+      log.error(e.getMessage(), e);
+    }
+    JWTClaimsSet claimsSet = new JWTClaimsSet.Builder()
+      .subject(xor(subject.getBytes()))
+      .issuer(ISSUER)
+      .issueTime(DateTime.now().toDate())
+      .expirationTime(DateTime.now().plusDays(expiryDays).toDate())
+      .build();
+    SignedJWT signedJWT = new SignedJWT(JWT_HEADER, claimsSet);
+    try {
+      signedJWT.sign(signer);
     } catch (JOSEException e) {
       log.error(e.getMessage(), e);
     }
-
-    return jwt.serialize();
+    return signedJWT.serialize();
   }
 
-  private static ReadOnlyJWTClaimsSet decode(String authHeader) throws NotAuthorizedException {
-
-    ReadOnlyJWTClaimsSet jwtClaimsSet = null;
+  private static JWTClaimsSet decode(String authHeader) throws NotAuthorizedException {
+    SignedJWT jwtClaimsSet = null;
+    JWTClaimsSet claimsSet = null;
     try {
-      jwtClaimsSet = SignedJWT.parse(getSerializedToken(authHeader)).getJWTClaimsSet();
-    } catch (ParseException e) {
+      jwtClaimsSet = SignedJWT.parse(authHeader);
+      JWSVerifier verifier = new MACVerifier(sharedSecret);
+      boolean verify = jwtClaimsSet.verify(verifier);
+      if (!verify) {
+        throw NotAuthorizedException.childBuilder().error("Unable to verify the token").build();
+      }
+      claimsSet = jwtClaimsSet.getJWTClaimsSet();
+    } catch (Exception e) {
       throw NotAuthorizedException.childBuilder().error("Invalid Token provided!").build();
     }
-    Date expiryDate = jwtClaimsSet.getExpirationTime();
+
+    Date expiryDate = claimsSet.getExpirationTime();
     Date now = DateTime.now().toDate();
 
     if (now.after(expiryDate)) {
       throw NotAuthorizedException.childBuilder().error("Token expired").build();
     }
-    return jwtClaimsSet;
-  }
-
-  private static String getSerializedToken(String authHeader) {
-    return authHeader;
+    return claimsSet;
   }
 
   private static String xor(final byte[] input) {
