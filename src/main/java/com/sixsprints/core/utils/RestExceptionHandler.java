47788762fee02d27b6738c6a1e5fd47663b2b9e0
AbhijeetAmanJPTC <99485551+AbhijeetAmanJPTC@users.noreply.github.com>
2022-02-14 14:02:09 +0530

    Message source service updated for bilingual messages through dto classes, Enum error message modification updated (#4)
    Co-authored-by: abhijeet_aman <abhijeet.aman@jpotokyo.co.jp>

--- src/main/java/com/sixsprints/core/utils/RestExceptionHandler.java
+++ src/main/java/com/sixsprints/core/utils/RestExceptionHandler.java
@@ -1,6 +1,7 @@
 
 package com.sixsprints.core.utils;
 
+import java.util.Arrays;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Locale;
@@ -13,6 +14,7 @@ import javax.validation.Path.Node;
 import org.apache.commons.collections.CollectionUtils;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.util.StringUtils;
 import org.springframework.validation.FieldError;
 import org.springframework.validation.ObjectError;
@@ -20,6 +22,7 @@ import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
+import com.fasterxml.jackson.databind.exc.InvalidFormatException;
 import com.sixsprints.core.exception.BaseException;
 import com.sixsprints.core.exception.BaseRuntimeException;
 import com.sixsprints.core.service.MessageSourceService;
@@ -76,11 +79,18 @@ public abstract class RestExceptionHandler {
     if (objectError instanceof FieldError) {
       field = ((FieldError) objectError).getField();
     }
-    String code = objectError.getCode();
-    boolean isEmpty = "NotNull".equalsIgnoreCase(code) || "NotEmpty".equalsIgnoreCase(code);
-    String errorDescription = isEmpty ? "not entered." : "invalid. " + field + " " + objectError.getDefaultMessage();
-    String error = "Request Parameter anomaly. " + field + " is " + errorDescription;
-    log.error(error);
+    FieldError fieldError = (FieldError) objectError;
+    String error = null;
+    error = fieldError.getDefaultMessage();
+    if(StringUtils.hasText(error)) {
+      error = getErrorMessage(error, List.of(), locale);
+    } else {
+      String code = objectError.getCode();
+      boolean isEmpty = "NotNull".equalsIgnoreCase(code) || "NotEmpty".equalsIgnoreCase(code);
+      String errorDescription = isEmpty ? "not entered." : "invalid. " + field + " " + objectError.getDefaultMessage();
+      error = "Request Parameter anomaly. " + field + " is " + errorDescription;
+      log.error(error);
+    }
     return RestUtil.errorResponse(null, error, HttpStatus.BAD_REQUEST);
   }
 
@@ -111,5 +121,22 @@ public abstract class RestExceptionHandler {
 
   protected String getErrorMessage(String key) {
     return getErrorMessage(key, Locale.ENGLISH);
+  }
+  
+  @ExceptionHandler(HttpMessageNotReadableException.class)
+  public ResponseEntity<?> handleConstraintVoilationException(HttpMessageNotReadableException exception,
+    HttpServletRequest request) {
+    String genericMessage = "Unacceptable Json " + exception.getMessage();
+    String errorDetails = genericMessage;
+    if (exception.getCause() instanceof InvalidFormatException) {
+      InvalidFormatException invalidFormatException = (InvalidFormatException) exception.getCause();
+      if (invalidFormatException.getTargetType().isEnum()) {
+        errorDetails = String.format("Invalid enum value: '%s' for the field: '%s'. The value must be one of: %s.",
+          invalidFormatException.getValue(),
+          invalidFormatException.getPath().get(invalidFormatException.getPath().size() - 1).getFieldName(),
+          Arrays.toString(invalidFormatException.getTargetType().getEnumConstants()));
+      }
+    }
+    return RestUtil.errorResponse(null, errorDetails, HttpStatus.BAD_REQUEST);
   }
 }
