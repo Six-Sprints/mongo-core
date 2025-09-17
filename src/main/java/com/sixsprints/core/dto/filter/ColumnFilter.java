4ed39fb6fdfed69934d4a2868032aab4258f419c
Karan Gujral <kgujral@gmail.com>
2025-09-17 13:23:56 +0900

    added swagger docs for columnfilter

--- src/main/java/com/sixsprints/core/dto/filter/ColumnFilter.java
+++ src/main/java/com/sixsprints/core/dto/filter/ColumnFilter.java
@@ -5,6 +5,9 @@ import com.fasterxml.jackson.annotation.JsonSubTypes;
 import com.fasterxml.jackson.annotation.JsonTypeInfo;
 
 import lombok.Data;
+import io.swagger.v3.oas.annotations.media.DiscriminatorMapping;
+import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.NotNull;
 
 @JsonTypeInfo(use = JsonTypeInfo.Id.NAME, include = JsonTypeInfo.As.EXISTING_PROPERTY,
     property = "filterType", visible = true)
@@ -15,9 +18,18 @@ import lombok.Data;
     @JsonSubTypes.Type(value = SearchColumnFilter.class, name = "TEXT"),
     @JsonSubTypes.Type(value = ExactMatchColumnFilter.class, name = "EXACT")})
 @JsonIgnoreProperties(ignoreUnknown = true)
+@Schema(discriminatorProperty = "filterType",
+    discriminatorMapping = {
+        @DiscriminatorMapping(value = "NUMBER", schema = NumberColumnFilter.class),
+        @DiscriminatorMapping(value = "SET", schema = SetColumnFilter.class),
+        @DiscriminatorMapping(value = "BOOLEAN", schema = BooleanColumnFilter.class),
+        @DiscriminatorMapping(value = "DATE", schema = DateColumnFilter.class),
+        @DiscriminatorMapping(value = "TEXT", schema = SearchColumnFilter.class),
+        @DiscriminatorMapping(value = "EXACT", schema = ExactMatchColumnFilter.class)})
 @Data
 public abstract class ColumnFilter {
 
+  @NotNull
   ColumnFilterTypeEnum filterType;
 
 }
